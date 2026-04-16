a08b858c2e397d01016926153df65d3ff3ff4125
Kainat Nadeem <bc220422631kna@vu.edu.pk>
2026-04-16 11:20:17 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
 import { Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import About from './pages/About'
 import Courses from './pages/Courses'
 import Contact from './pages/Contact'
+import AIMentor from './pages/AIMentor'
+import Dashboard from './pages/Dashboard'
 
 export default function App() {
   return (
-    <div className="min-h-screen flex flex-col bg-midnight">
-      <Navbar />
-      <main className="flex-1">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/courses" element={<Courses />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
+    <AuthProvider>
+      <div className="min-h-screen flex flex-col bg-midnight">
+        <Navbar />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/courses" element={<Courses />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/ai-mentor" element={<AIMentor />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </AuthProvider>
   )
 }
